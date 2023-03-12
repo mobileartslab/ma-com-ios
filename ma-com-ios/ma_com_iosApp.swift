3e5edceb50cabe49ab9cab1449976a46401950c1
Nick Sophinos <nick@wdeanmedical.com>
2023-03-11 23:47:01 -0500

    work on login screen

--- ma-com-ios/ma_com_iosApp.swift
+++ ma-com-ios/ma_com_iosApp.swift
@@ -1,20 +1,11 @@
-//
-//  ma_com_iosApp.swift
-//  ma-com-ios
-//
-//  Created by Nick Sophinos on 3/6/23.
-//
-
 import SwiftUI
 
 @main
 struct ma_com_iosApp: App {
-    let persistenceController = PersistenceController.shared
 
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
-                .environment(\.managedObjectContext, persistenceController.container.viewContext)
-        }
+  var body: some Scene {
+    WindowGroup {
+      LoginView()
     }
+  }
 }
